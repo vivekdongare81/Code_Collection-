9dfbf3c7f14ad59dc36a2637d958990b28eaf9fe
Vivek <dongarevivek81@gmail.com>
2022-01-05 23:15:07 +0530

    swd

---
+++ React_Files/my-keepit-app/src/components/Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        {" "}
+        <DeleteForeverIcon />{" "}
+      </button>
+    </div>
+  );
+}
+
+export default Note;
